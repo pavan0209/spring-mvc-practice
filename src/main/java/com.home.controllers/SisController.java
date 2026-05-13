2c43d57560a783966f066d0c1887012d40bec6ec
pavan0209 <pavanasonawane@gmail.com>
2026-05-13 22:02:17 +0530

    added SisController to serve '/get-makeup' and '/book' request

---
+++ src/main/java/com.home.controllers/SisController.java
@@ -0,0 +1,21 @@
+package com.home.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SisController {
+
+    @ResponseBody
+    @RequestMapping("/get-makeup")
+    public String getMakeUpKit() {
+        return "take this make up kit......";
+    }
+
+    @ResponseBody
+    @RequestMapping("/book")
+    public String getBooks() {
+        return "Science Book";
+    }
+}
